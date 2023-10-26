f089930497e832e83c0cc1216a27a16a8eaf4708
asa93 <asa93@live.fr>
2023-10-27 01:36:48 +0200

    add logo snowflake

--- components/sidebar.js
+++ components/sidebar.js
@@ -17,10 +17,22 @@ import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import { ACTION } from "next/dist/client/components/app-router-headers";
+
 export default function SideBar({ children, home, title }) {
   return (
     <Drawer variant="persistent" anchor={"left"} open={true}>
       <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AcUnitIcon fontSize="large" />
+            </ListItemIcon>
+            <ListItemText primary="CityClim" />
+          </ListItemButton>
+        </ListItem>
+
         <ListItem disablePadding>
           <Link href={`/`}>
             <ListItemButton>
