08bce5d399a2c35ea4e6f4bfdc401cdb72f944ad
asa93 <asa93@live.fr>
2023-10-28 19:35:39 +0200

    Units page

---
+++ pages/api/units.js
@@ -0,0 +1,48 @@
+import { createClient } from "@supabase/supabase-js";
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+export default async (req, res) => {
+  const { account, place } = req.query;
+
+  if (req.method == "GET") {
+    let { data, error } = await supabase
+      .from("Units")
+      .select("name, Places!inner ( name, Accounts!inner (name) )  ")
+      .ilike("Places.name", `%${place}%`)
+      .ilike("Places.Accounts.name", `%${account}%`)
+      .limit(50);
+
+    if (error) return res.status(400).json({ data: null, error: error });
+    else {
+      data = data.map((r) => {
+        console.log(r);
+        return {
+          name: r.name,
+          place: r.Places.name,
+          account: r.Places.Accounts.name,
+        };
+      });
+      res.status(200).json(data);
+    }
+  } else if (req.method == "POST") {
+    //todo
+    const { name, account } = req.body;
+
+    const { data, error } = await supabase
+      .from("Units")
+      .insert([{ name: name, account: account }])
+      .select();
+
+    res.status(200).json({ error: null });
+  } else {
+    if (error)
+      return res
+        .status(400)
+        .json({ data: null, error: "method not authorized" });
+  }
+};
---
+++ pages/units.js
@@ -0,0 +1,155 @@
+import Layout from "../components/layout";
+
+import { getSortedPostsData } from "../lib/posts";
+
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Button, TextField } from "@mui/material";
+import axios from "axios";
+import useAxios from "axios-hooks";
+import React, { useState } from "react";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Autocomplete from "@mui/material/Autocomplete";
+import TablePagination from "@mui/material/TablePagination";
+
+import LinearProgress from "@mui/material/LinearProgress";
+import Alert from "@mui/material/Alert";
+import Divider from "@mui/material/Divider";
+
+export default function Component() {
+  const [showForm, setShowForm] = useState(false);
+
+  const [newName, setNewName] = useState("");
+  const [newAccountId, setNewAccountId] = useState("");
+
+  const [accountFilter, setAccountFilter] = useState("");
+  const [placeFilter, setPlaceFilter] = useState("");
+
+  const [accountFilter2, setaccountFilter2] = useState("");
+
+  const [{ data: units, loading, error }, refetch] = useAxios({
+    url: "/api/units",
+    params: { account: accountFilter, place: placeFilter },
+  });
+
+  const [{ data: accounts, loading: loadingAcc, error: errorAcc }] = useAxios({
+    url: "/api/accounts",
+    params: { name: accountFilter2 },
+  });
+
+  const handleSave = async () => {
+    if (newName)
+      await axios.post("/api/places", {
+        name: newName,
+        account: newAccountId,
+      });
+  };
+
+  return (
+    <Layout home title={"Machines"}>
+      {loading && <LinearProgress />}
+      {error && <Alert severity="error">{error.message}</Alert>}
+
+      <Button variant="contained" onClick={() => setShowForm(!showForm)}>
+        <AddCircleIcon />
+      </Button>
+
+      {showForm && (
+        <>
+          <h2>Ajouter machine</h2>
+
+          <TextField label="Nom" onChange={(e) => setNewName(e.target.value)} />
+
+          <Autocomplete
+            disablePortal
+            options={accounts.map((acc) => {
+              return { label: acc.name, id: acc.id };
+            })}
+            sx={{ width: 300 }}
+            renderInput={(params) => <TextField {...params} label="Client" />}
+            onInputChange={(e, newInputValue) => {
+              setaccountFilter2(newInputValue);
+            }}
+            onChange={(event, newValue) => {
+              setNewAccountId(newValue ? newValue.id : null);
+            }}
+            filterOptions={(x) => x}
+          />
+
+          <Button variant="contained" onClick={handleSave}>
+            Enregistrer
+          </Button>
+
+          <Divider />
+          <br />
+        </>
+      )}
+
+      {units && (
+        <>
+          <TableContainer component={Paper}>
+            <Table sx={{}} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Nom</TableCell>
+
+                  <TableCell align="right">
+                    <TextField
+                      label="Local"
+                      onChange={(e) => setPlaceFilter(e.target.value)}
+                    />
+                  </TableCell>
+
+                  <TableCell align="right">
+                    <TextField
+                      label="Client"
+                      onChange={(e) => setAccountFilter(e.target.value)}
+                    />
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody>
+                {units.map((r) => (
+                  <TableRow
+                    key={r.name}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {r.name}
+                    </TableCell>
+                    <TableCell align="right">{r.place}</TableCell>
+                    <TableCell align="right">{r.account}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[50]}
+            component="div"
+            count={units.length}
+            rowsPerPage={50}
+            page={0}
+            onPageChange={() => {}}
+            onRowsPerPageChange={() => {}}
+          />
+        </>
+      )}
+    </Layout>
+  );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
