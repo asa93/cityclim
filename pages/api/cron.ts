2a607d22ba54c528129ab0a4b4ab2721bfe32fcd
asa93 <asa93@live.fr>
2024-01-01 19:17:21 +0100

    update cron

--- pages/api/cron.ts
+++ pages/api/cron.ts
@@ -1,11 +1,30 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { createClient } from "@supabase/supabase-js";
 
-export default function handler(
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
+);
+
+
+export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+
+  const query = supabase
+  .from("Maintenances")
+  .select(
+    "* , Units!inner( id, reference, serial, Places!inner (name, Accounts!inner(name) ), References!inner(checkpoints) )"
+  )
+
+  .limit(2);
+
+  const { data, error } = await query;
+
+  if(!error)
   response.status(200).json({
-    body: request.body,
+    body: data,
     query: request.query,
     cookies: request.cookies,
   });
