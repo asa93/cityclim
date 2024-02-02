22b6a45f9012db24153db218408a7bfc41f024ac
asa93 <asa93@live.fr>
2024-02-02 17:19:16 +0100

    fix type error

--- types/types.ts
+++ types/types.ts
@@ -8,7 +8,7 @@ export type Place = Database["public"]["Tables"]["Places"]["Row"] & { account: s
 
 export type Unit = Database["public"]["Tables"]["Units"]["Row"] & { account: string, place: string }
 
-export type Maintenance = Database["public"]["Tables"]["Maintenances"]["Row"] & { unit_name: string, account: string, place: string, reference: string, serial: string, checkpoints: object, checkpoints_ref: object }
+export type Maintenance = Database["public"]["Tables"]["Maintenances"]["Row"] & { estimate_id: string, unit_name: string, account: string, place: string, reference: string, serial: string, checkpoints: object, checkpoints_ref: object }
 
 export type Reference = Database["public"]["Tables"]["References"]["Row"]
 
